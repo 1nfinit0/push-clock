7fee6e43e44ab56cb5d3177228950d59d93567cc
tobi <noggnzzz@gmail.com>
2024-10-20 20:10:24 -0500

    Actualizar random_push.py

--- random_push.py
+++ random_push.py
@@ -2,6 +2,7 @@ import random
 
 # Genera un número aleatorio entre 2 y 10
 random_count = random.randint(2, 10)
+print("el número es: ", random_count)
 
 # Escribe el número aleatorio en un archivo
 with open('count.txt', 'w') as f:
